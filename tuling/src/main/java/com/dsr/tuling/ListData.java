ed5da0dc4b4c5fc740f3509204803c761cec1843
168367406 <168367406@qq.com>
2015-12-15 23:43:52 +0800

    修改了ListData中的代码kkk

--- tuling/src/main/java/com/dsr/tuling/ListData.java
+++ tuling/src/main/java/com/dsr/tuling/ListData.java
@@ -3,11 +3,13 @@ package com.dsr.tuling;
 /**
  * Created by Administrator on 2015/12/13.
  */
+//test
 public class ListData {
     private String content;
     public static final int SEND=1;
     public static final int RECEIVER=2;
     private int flag;
+    int a=9;
 
     public ListData(String content,int flag) {
         setContent(content);
